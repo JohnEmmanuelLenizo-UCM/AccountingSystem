020c80967c07c78e39c9a05ae49139a18be074a0
JohnEmmanuelLenizo-UCM <johnemmanuellenizo99@gmail.com>
2025-11-18 19:33:41 +0800

    update

---
+++ accountingSystem/Main.java
@@ -0,0 +1,11 @@
+package accountingSystem;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Frame frame = new Frame();
+            frame.show();
+        });
+    }
+}
